47148a8e5432b6c6e5662af95450c85e7f61f3d6
tbagriyanik <tbagriyanik@20c31af0-bc10-11de-82ee-51df2961d3cf>
2010-07-20 12:49:29 +0000

    GNU Lisans bilgisi eklendi

--- addComment2.php
+++ addComment2.php
@@ -1,4 +1,18 @@
 <?php
+/*
+eOgr - elearning project
+
+Developer Site: http://yunus.sourceforge.net
+Demo Site:		http://www.tuzlaatl.k12.tr/eogr
+Source Track:	http://eogr.googlecode.com 
+Support:		http://www.ohloh.net/p/eogr
+
+This project is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or any later version. See the GNU
+Lesser General Public License for more details.
+*/
 	session_start();
 	header("Content-Type: text/html; charset=iso-8859-9");          
 
--- lessons.php
+++ lessons.php
@@ -1,4 +1,19 @@
 <?php
+/*
+eOgr - elearning project
+
+Developer Site: http://yunus.sourceforge.net
+Demo Site:		http://www.tuzlaatl.k12.tr/eogr
+Source Track:	http://eogr.googlecode.com 
+Support:		http://www.ohloh.net/p/eogr
+
+This project is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or any later version. See the GNU
+Lesser General Public License for more details.
+*/
+
     header("Expires: Sun, 1 Jan 2000 12:00:00 GMT");
     header("Last-Modified: " . gmdate("D, d M Y H:i:s") . "GMT");
     header("Cache-Control: no-store, no-cache, must-revalidate");
