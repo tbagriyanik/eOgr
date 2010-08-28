97af13335ac8963915d99c4dc95dc665930a4b91
tbagriyanik <tbagriyanik@20c31af0-bc10-11de-82ee-51df2961d3cf>
2010-08-28 22:33:53 +0000

    ops

---
+++ trunk/_siteLock.php
@@ -0,0 +1,10 @@
+<?php
+	//eogr - Maintenance File (Bakim Dosyasi)
+	//[EN]
+	//if you give a name like "siteLock.php" to this file, site is CLOSED.
+	//if you give a name like "_siteLock.php" to this file, site is FREE to use.
+	//[TR]
+	//Eðer "siteLock.php" ismi verirlirse, site KAPANIR.
+	//Eðer "_siteLock.php" ismi verilirse, site kullanýma AÇIK olur.
+	echo "NULL";
+?>
