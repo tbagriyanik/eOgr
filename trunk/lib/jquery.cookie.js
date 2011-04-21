e1e3c83d5649c7bade9b8d07a93cb1faef37355e
tbagriyanik <tbagriyanik@20c31af0-bc10-11de-82ee-51df2961d3cf>
2011-04-21 16:56:03 +0000

    ip takibi için whois eklendi

--- trunk/lib/jquery.cookie.js
+++ trunk/lib/jquery.cookie.js
@@ -90,3 +90,24 @@ jQuery.cookie = function(name, value, options) {
         return cookieValue;
     }
 };
+
+function degerOku(isim) {
+	if (document.cookie.length > 0) {
+		c_start = document.cookie.indexOf(isim + "=");
+		if (c_start != -1) {
+			c_start = c_start + isim.length + 1;
+			c_end   = document.cookie.indexOf(";",c_start);
+			if (c_end == -1) {
+				c_end = document.cookie.length;
+			}
+			return unescape(document.cookie.substring(c_start,c_end));
+		} 
+	}
+	return "";
+}
+
+function degerYaz(isim, deger, expiredays) {
+	var exdate = new Date();
+	exdate.setDate(exdate.getDate() + expiredays);
+	document.cookie = isim + "=" + escape(deger) + ((expiredays === null) ? "" : ";expires=" + exdate.toGMTString());
+}		
